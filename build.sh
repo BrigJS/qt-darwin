92a83f3ae06c51b84eae88a733c677bab3e6e90f
Fred Chien <cfsghost@gmail.com>
2017-06-19 09:40:36 +0800

    packaging library

--- build.sh
+++ build.sh
@@ -21,7 +21,13 @@ mv -f qt-darwin.app/Contents/Resources/qml Resources/
 # modify run-time path for library searching
 install_name_tool -add_rpath @loader_path/../../Frameworks PlugIns/platforms/libqcocoa.dylib
 
+# Packaging
+tar -zcvf library.tgz Frameworks PlugIns Resources
+
 # Clean
+rm -fr Frameworks
+rm -fr PlugIns
+rm -fr Resources
 rm -fr qt-darwin.app
 rm -fr qrc_qml.cpp
 rm -fr Makefile
